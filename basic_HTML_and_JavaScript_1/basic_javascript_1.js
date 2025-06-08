1593f85e3c448ed8322e6539b1eacc2991f0a975
ColorlessSaber <110745660+ColorlessSaber@users.noreply.github.com>
2025-06-08 14:08:23 -0700

    Add new folder
    Added a new folder basic_HTML_and_JavaScript_1 that contains basic examples of HTML and JavaScript.

---
+++ basic_HTML_and_JavaScript_1/basic_javascript_1.js
@@ -0,0 +1,6 @@
+alert("Hello, world! I am an alert box!");
+function kiss_irish(){
+	var string = "Kiss me, I'm Irish!";
+	document.getElementById("irish").innerHTML = string;
+	document.getElementById("irish").style.color = "green";
+}
