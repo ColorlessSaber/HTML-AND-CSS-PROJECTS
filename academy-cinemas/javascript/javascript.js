2876edc5e7c5703a497300854aba9e7ac192dfb9
ColorlessSaber <110745660+ColorlessSaber@users.noreply.github.com>
2025-06-27 13:14:43 -0700

    Added jQuery to stylize the navbar.

--- academy-cinemas/javascript/javascript.js
+++ academy-cinemas/javascript/javascript.js
@@ -33,3 +33,39 @@ function displaySelectedMovieOptions() {
 function buyTickets() {
     displaySelectedMovieOptions();
 }
+
+// *** JQUERY ***
+// shrinks header size when document is scrolled down by 80 pixels
+$(document).on("scroll", function(){
+    // when the webpage is scrolled down from the top by 50px this
+    // if statement will trigger
+    if ($(document).scrollTop() > 50) {
+        // Once the 50px requirment has been met add the
+        // nav-shrink class selector to the same HTML element
+        // that has the nav class
+        $("nav").addClass("nav-shrink");
+        // Adjust the position of the mobile drop menu
+        // to accommodate the new height decrease
+        $("div.navbar-collapse").css("margin-top", "-6px");
+    } else {
+        // if the webpage has not been scrolled down or
+        // is back at the top of the nav-shrink class seelctor
+        // is removed from the HTML element with the nav
+        // class selector
+        $("nav").removeClass("nav-shrink");
+        // The margin for the drop down menu is now
+        // return to it's orginal amount
+        $("div.navbar-collapse").css("margin-top", "14px");
+    }
+
+});
+
+// close modile menu when a navigation link is clicked
+$(document).ready(function (){
+    // on click when and element contains just the nav-link class and not the dropdown-toggle and then
+    // also close when an element with the class .dropdown-item (each movie link) has been clicked
+    $(".navbar-nav").on('click', '.nav-link:not(".dropdown-toggle"), .dropdown-item', function(){
+        // collapse the navbar when a link or dropdown item is clicked
+        $(".navbar-collapse").collapse('hide');
+    });
+});
